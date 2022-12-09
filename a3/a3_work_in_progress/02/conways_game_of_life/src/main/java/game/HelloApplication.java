f4a001cf1f5d1dfcd30f34226b76769fab747ba9
Darnol <dom.arnold@bluewin.ch>
2022-12-09 15:19:06 +0100

    Remove FXML support

--- a3/a3_work_in_progress/02/conways_game_of_life/src/main/java/game/HelloApplication.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/src/main/java/game/HelloApplication.java
@@ -1,19 +1,43 @@
 package game;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static final int HEIGHT = 400;
+    private static final int WIDTH = 600;
+    private static final int MARGIN_OUTER = 10;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) {
+
+        stage.setTitle("Test JavaFX");
+
+        GridPane root = new GridPane();
+        root.setGridLinesVisible(true);
+        root.setStyle("-fx-background-color: green");
+        root.setHgap(MARGIN_OUTER);
+        root.setVgap(MARGIN_OUTER);
+        root.setPadding(new Insets(MARGIN_OUTER));
+
+        Button buttonOne = new Button("Click me.");
+        buttonOne.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Button clicked.");
+            }
+        });
+        root.getChildren().add(buttonOne);
+
+        stage.setScene(new Scene(root, WIDTH, HEIGHT));
         stage.show();
     }
 
