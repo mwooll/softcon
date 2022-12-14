37c0d40ae22bccfe81b541fabfedd451c817cbd8
mwooll <mark.woolley@uzh.ch>
2022-12-14 16:30:48 +0100

    added (regarding current class diagram) complete Cell and initial Grid
    todo: right methods for Grid

---
+++ a3/a3_work_in_progress/02/conways_game_of_life/src/cell/Cell.java
@@ -0,0 +1,53 @@
+package cell;
+
+import player.PlayerColor;
+
+public class Cell {
+    private player.PlayerColor currentState;
+    private player.PlayerColor nextState;
+    private boolean wasChanged;
+
+    public Cell() {
+        currentState = player.PlayerColor.WHITE;
+        wasChanged = false;
+    }
+
+    /**
+     * @pre currentState != player.PlayerColor.WHITE
+     */
+    public void die() {
+        nextState = player.PlayerColor.WHITE;
+        wasChanged = true;
+    }
+
+    /**
+     * @pre currentState != player.PlayerColor.WHITE
+     */
+    public void instantDeath() {
+        currentState = player.PlayerColor.WHITE;
+    }
+
+    /**
+     * @pre currentState == player.PlayerColor.WHITE
+     */
+    public void arrive(player.PlayerColor newState) {
+        nextState = newState;
+        wasChanged = true;
+    }
+
+    /**
+     * @pre currentState == player.PlayerColor.WHITE
+     */
+    public void instantBirth(player.PlayerColor newState) {
+        currentState = newState;
+    }
+
+    public PlayerColor getState() { return currentState; }
+
+    /**
+     * @pre wasChanged == true
+     */
+    public void resetWasChanged() {
+        wasChanged = false;
+    }
+}
