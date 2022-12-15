3d3b20a8c256ff0cc44b3c42bc6e63799231473a
mwooll <mark.woolley@uzh.ch>
2022-12-15 15:06:31 +0100

    added note that pColor must not be WHITE

--- a3/a3_work_in_progress/02/conways_game_of_life/src/player/Player.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/src/player/Player.java
@@ -4,6 +4,9 @@ public class Player {
     private final String aName;
     private final PlayerColor aColor;
 
+    /**
+     * @param pColor must not be PlayerColor.WHITE
+     */
     public Player(String pName, PlayerColor pColor) {
         aName = pName;
         aColor = pColor;
