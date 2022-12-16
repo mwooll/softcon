b097fad8428192dbddc3968b979c1abbdf6eaa79
Darnol <dom.arnold@bluewin.ch>
2022-12-16 17:33:04 +0100

    Grid add Iterator
    Add Tests for Iterator

--- a3/a3_work_in_progress/02/conways_game_of_life/src/cell/Grid.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/src/cell/Grid.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Grid implements Iterator<Cell> {
+public class Grid implements Iterable<Cell> {
     private final int aWidth;
     private final int aHeight;
 
-    private final List<List<Cell>> aGrid = new ArrayList<List<Cell>>();
+    private final List<List<Cell>> aGrid = new ArrayList<>();
 
     public Grid(int pWidth, int pHeight) {
         aWidth = pWidth;
@@ -21,7 +21,6 @@ public class Grid implements Iterator<Cell> {
                 aGrid.get(row).add(new Cell());
             }
         }
-
     }
 
     public int getWidth() { return aWidth; }
@@ -30,13 +29,15 @@ public class Grid implements Iterator<Cell> {
     /**
      * 0 Indexed
      * @throws IndexOutOfBoundsException when the indices are out of bounds
-     * @pre pRow and pCol must be positive
+     * @pre pRow and pCol must be bigger than 0
      * @return The Cell at row pRow and col pCol
      */
     public Cell getCell(int pRow, int pCol) {
-        assert pRow > 0 && pCol > 0;
+        assert pRow >= 0 && pCol >= 0;
         return aGrid.get(pRow).get(pCol);
     }
+
+
 
     public void generateNextGeneration() {
 
@@ -47,12 +48,46 @@ public class Grid implements Iterator<Cell> {
     }
 
     @Override
-    public boolean hasNext() {
-        return false;
+    public Iterator<Cell> iterator() {
+        return new iteratorTopLeftBottomRight();
     }
 
-    @Override
-    public Cell next() {
-        return null;
+    /**
+     *
+     */
+    class iteratorTopLeftBottomRight implements Iterator<Cell> {
+        private int ctRow, ctCol = 0;
+        private int ctIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+
+            // The first Cell must always be looked at
+            if (ctRow == 0 && ctCol == 0) {
+                return true;
+            }
+
+            int nextIndex = (ctRow * aWidth) + ctCol + 1;
+
+            return nextIndex <= aHeight * aWidth;
+
+        }
+
+        @Override
+        public Cell next() {
+
+            Cell currentCell = getCell(ctRow, ctCol);
+
+            boolean lastCol = ctCol == aWidth-1;
+
+            // either one column to the right if possible, or otherwise reset ctCol and go one row down
+            if (!lastCol) {ctCol += 1;} else {
+                ctCol = 0;
+                ctRow += 1;
+            }
+
+            return currentCell;
+
+        }
     }
 }
--- a3/a3_work_in_progress/02/conways_game_of_life/src/scratch/scratchpad.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/src/scratch/scratchpad.java
@@ -1,38 +1,39 @@
 package scratch;
 
+import cell.Cell;
+import cell.Grid;
 import gamemodel.*;
 import javafx.scene.paint.Color;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class scratchpad {
 
     public static void main(String[] args) {
 
-//        List<String> mylist = Arrays.asList("a","a","b");
-//
-//        System.out.println(
-//                mylist.stream().filter(string -> string.equals("c")).count()
-//        );
+        int w = 1;
+        int h = 1;
+        Grid g = new Grid(w,h);
 
-//        List<Color> aColors = Arrays.asList(new Color[3]);
-//        aColors.set(0, Color.BLUE);
-//        aColors.set(1, Color.RED);
-//        aColors.set(2, Color.GREEN);
-//
-//        HashSet<Color> aHashSet = new HashSet<Color>(aColors);
-//        System.out.println(aHashSet);
-//        System.out.println(aHashSet.size());
-//        System.out.println(aColors.size());
-//        System.out.println(aColors.contains(null));
+        String tmp = "";
 
-        String mytext = "25.5";
-        int myint = Integer.parseInt(mytext);
-        System.out.println(myint);
+        int ct = 1;
+        for (Cell c : g) {
 
+            String cstr = c.getState().getColorName();
 
+            if (ct%w == 0) {
+                tmp += String.format("%s\n", cstr);
+            } else {
+                tmp += String.format("%s ", cstr);
+            }
+            ct += 1;
+
+        }
+        System.out.println(tmp);
 
     }
 
