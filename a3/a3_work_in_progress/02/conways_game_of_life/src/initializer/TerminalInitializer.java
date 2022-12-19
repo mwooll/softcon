1889e2be06d75ad4d18147d8ad51cfccc368f005
mwooll <mark.woolley@uzh.ch>
2022-12-19 13:49:46 +0100

    TerminalInitializer implements Initializer

--- a3/a3_work_in_progress/02/conways_game_of_life/src/initializer/TerminalInitializer.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/src/initializer/TerminalInitializer.java
@@ -1,4 +1,28 @@
 package initializer;
 
-public class TerminalInitializer {
+import cell.Grid;
+import player.PlayerColor;
+
+import java.util.ArrayList;
+
+public class TerminalInitializer implements Initializer{
+    @Override
+    public String choosePlayerName() {
+        return null;
+    }
+
+    @Override
+    public PlayerColor choosePlayerColor() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> chooseGridDimensions() {
+        return null;
+    }
+
+    @Override
+    public Grid createStartingConfiguration() {
+        return null;
+    }
 }
