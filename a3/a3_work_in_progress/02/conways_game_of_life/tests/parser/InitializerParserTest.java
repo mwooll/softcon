6714eb30a59813ef0f40ff3e97a0f77dff12903f
mwooll <mark.woolley@uzh.ch>
2022-12-19 16:06:10 +0100

    added tests for validatePlayerColor

--- a3/a3_work_in_progress/02/conways_game_of_life/tests/parser/InitializerParserTest.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/tests/parser/InitializerParserTest.java
@@ -1,6 +1,7 @@
 package parser;
 
 import org.junit.jupiter.api.Test;
+import player.PlayerColor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,5 +36,33 @@ public class InitializerParserTest {
 
         InitializerParser nameParser = new InitializerParser();
         assertFalse(nameParser.validatePlayerName(usedNames, newName));
+    }
+
+    @Test
+    public void testValidateColorWhite() {
+        List<PlayerColor> pColors = new ArrayList<PlayerColor>();
+        PlayerColor pColor = PlayerColor.WHITE;
+
+        InitializerParser colorParser = new InitializerParser();
+        assertFalse(colorParser.validateColor(pColors, pColor));
+    }
+
+    @Test
+    public void testValidateColorValid() {
+        List<PlayerColor> pColors = new ArrayList<PlayerColor>();
+        PlayerColor pColor = PlayerColor.ORANGE;
+
+        InitializerParser colorParser = new InitializerParser();
+        assertTrue(colorParser.validateColor(pColors, pColor));
+    }
+
+    @Test
+    public void testValidateColorDuplicate() {
+        List<PlayerColor> pColors = new ArrayList<PlayerColor>();
+        pColors.add(PlayerColor.GREEN);
+        PlayerColor pColor = PlayerColor.GREEN;
+
+        InitializerParser colorParser = new InitializerParser();
+        assertFalse(colorParser.validateColor(pColors, pColor));
     }
 }
