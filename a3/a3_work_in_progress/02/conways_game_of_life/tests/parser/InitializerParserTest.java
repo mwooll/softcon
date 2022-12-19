a5e75fda8205ab048af908f905321c05cf8cbbca
mwooll <mark.woolley@uzh.ch>
2022-12-19 16:24:16 +0100

    added tests for validateHeight and ValidateWidth

--- a3/a3_work_in_progress/02/conways_game_of_life/tests/parser/InitializerParserTest.java
+++ a3/a3_work_in_progress/02/conways_game_of_life/tests/parser/InitializerParserTest.java
@@ -12,7 +12,7 @@ public class InitializerParserTest {
 
     @Test
     public void testValidatePlayerNameValid() {
-        List<String> usedNames = new ArrayList<String>();
+        List<String> usedNames = new ArrayList<>();
         String newName = "new Name";
 
         InitializerParser nameParser = new InitializerParser();
@@ -21,7 +21,7 @@ public class InitializerParserTest {
 
     @Test
     public void testValidatePlayerNameDuplicate() {
-        List<String> usedNames = new ArrayList<String>();
+        List<String> usedNames = new ArrayList<>();
         usedNames.add("Name");
         String newName = "Name";
 
@@ -31,7 +31,7 @@ public class InitializerParserTest {
 
     @Test
     public void testValidatePlayerNameForbiddenSymbol() {
-        List<String> usedNames = new ArrayList<String>();
+        List<String> usedNames = new ArrayList<>();
         String newName = "!+*ç";
 
         InitializerParser nameParser = new InitializerParser();
@@ -40,7 +40,7 @@ public class InitializerParserTest {
 
     @Test
     public void testValidateColorWhite() {
-        List<PlayerColor> pColors = new ArrayList<PlayerColor>();
+        List<PlayerColor> pColors = new ArrayList<>();
         PlayerColor pColor = PlayerColor.WHITE;
 
         InitializerParser colorParser = new InitializerParser();
@@ -49,7 +49,7 @@ public class InitializerParserTest {
 
     @Test
     public void testValidateColorValid() {
-        List<PlayerColor> pColors = new ArrayList<PlayerColor>();
+        List<PlayerColor> pColors = new ArrayList<>();
         PlayerColor pColor = PlayerColor.ORANGE;
 
         InitializerParser colorParser = new InitializerParser();
@@ -58,11 +58,74 @@ public class InitializerParserTest {
 
     @Test
     public void testValidateColorDuplicate() {
-        List<PlayerColor> pColors = new ArrayList<PlayerColor>();
+        List<PlayerColor> pColors = new ArrayList<>();
         pColors.add(PlayerColor.GREEN);
         PlayerColor pColor = PlayerColor.GREEN;
 
         InitializerParser colorParser = new InitializerParser();
         assertFalse(colorParser.validateColor(pColors, pColor));
+    }
+
+    @Test
+    public void testValidateHeightValid() {
+        int maxHeight = 100;
+        int minHeight = 10;
+        InitializerParser heightParser = new InitializerParser();
+
+        assertTrue(heightParser.validateHeight(maxHeight, minHeight, "10"));
+        assertTrue(heightParser.validateHeight(maxHeight, minHeight, "50"));
+        assertTrue(heightParser.validateHeight(maxHeight, minHeight, "100"));
+    }
+
+    @Test
+    public void testValidateHeightInvalid() {
+        int maxHeight = 200;
+        int minHeight = 0;
+        InitializerParser heightParser = new InitializerParser();
+
+        assertFalse(heightParser.validateHeight(maxHeight, minHeight, "-1"));
+        assertFalse(heightParser.validateHeight(maxHeight, minHeight, "2000"));
+    }
+
+    @Test
+    public void testValidateHeightFormatError() {
+        int maxHeight = 200;
+        int minHeight = 0;
+        InitializerParser heightParser = new InitializerParser();
+
+        assertFalse(heightParser.validateHeight(maxHeight, minHeight, "abc"));
+        assertFalse(heightParser.validateHeight(maxHeight, minHeight, "3."));
+    }
+
+    @Test
+    public void testValidateWidthValid() {
+        int maxWidth = 300;
+        int minWidth = 30;
+        InitializerParser widthParser = new InitializerParser();
+
+        assertTrue(widthParser.validateWidth(maxWidth, minWidth, "30"));
+        assertTrue(widthParser.validateWidth(maxWidth, minWidth, "150"));
+        assertTrue(widthParser.validateWidth(maxWidth, minWidth, "300"));
+    }
+
+    @Test
+    public void testValidateWidthInvalid() {
+        int maxWidth = 400;
+        int minWidth = 40;
+        InitializerParser widthParser = new InitializerParser();
+
+        assertFalse(widthParser.validateWidth(maxWidth, minWidth, "-27"));
+        assertFalse(widthParser.validateWidth(maxWidth, minWidth, "39"));
+        assertFalse(widthParser.validateWidth(maxWidth, minWidth, "401"));
+    }
+
+    @Test
+    public void testValidateWidthFormatError() {
+        int maxWidth = 400;
+        int minWidth = 40;
+        InitializerParser widthParser = new InitializerParser();
+
+        assertFalse(widthParser.validateWidth(maxWidth, minWidth, "dog"));
+        assertFalse(widthParser.validateWidth(maxWidth, minWidth, "4."));
     }
 }
